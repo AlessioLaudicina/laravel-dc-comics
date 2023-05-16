c12332cd59e8632ce67c962e04597f571c6fbf16
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-16 16:09:11 +0200

    creato un model con relativa migration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>DC COMICS</h1>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header class="py-3">
+  <div class="container text-center">
+    <a href="">
+      <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo">
+    </a>
+  </div>
+  <div class="container">
+    <a class="btn btn-success" href="">Add a new comic</a>
+  </div>
+</header>
